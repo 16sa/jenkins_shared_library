f59cf2e3f6050a97a4c6b4c3d316a8f1788bed23
16sa <safach2013@gmail.com>
2025-08-15 14:22:06 +0200

    update Quality Gate Status

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,9 +1,25 @@
 
-
-def call(credentialsId){
-
-waitForQualityGate abortPipeline: false, credentialsId: credentialsId
-
+def call(credentialsId) {
+    def qgStatus = 'UNKNOWN'
+    
+    // Set a timeout of 3 minutes (or whatever is appropriate)
+    timeout(time: 3, unit: 'MINUTES') { 
+        try {
+            // Wait for the quality gate status
+            qgStatus = waitForQualityGate()
+        } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+            // This block is executed if the timeout is reached
+            echo "Timeout reached. Quality Gate status is UNKNOWN."
+            qgStatus = 'UNKNOWN'
+        }
+    }
+    
+    // Continue the pipeline regardless of the status
+    if (qgStatus == 'OK') {
+        echo "Quality Gate passed!"
+    } else {
+        echo "Quality Gate did not pass. Proceeding to next stage."
+    }
 }
 
 
